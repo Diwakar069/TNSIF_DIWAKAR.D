c60e53ba147f559c5c1f5034207bb143b83dea41
HP <HP@LAPTOP-0OJ9S0RI.lan>
2025-10-01 17:35:01 +0530

    day 5 Non access modifier

--- Tnsif_Java_Diwakar/src/com/tnsif/nonaccessmodifiers/FinalDemo.java
+++ Tnsif_Java_Diwakar/src/com/tnsif/nonaccessmodifiers/FinalDemo.java
@@ -1,24 +1,23 @@
 package com.tnsif.nonaccessmodifiers;
 
-final class FinalDemo {
-	
-	final int Var = 100;
-	
-	final void display() {
-		System.out.println("show the value of max: "+Max);
-	}
+class FinalDemo {
 
-	public static void main(String[] args) {
-		FinalDemo fd = new FinalDemo();
-		fd.display();
-		fd.Var = 100;
+    final int Var = 100;
 
-	}
+    void display() {
+        System.out.println("show the value of Var: " + Var);
+    }
 
+    public static void main(String[] args) {
+        FinalDemo fd = new FinalDemo();
+        fd.display();
+        // fd.Var = 200; // ❌ Not allowed since Var is final
+    }
 }
 
-class Child extends FinalDemo{
-	void display() {
-		System.out.println("show the value of max: "+Max);
-	}
+class Child extends FinalDemo {
+    @Override
+    void display() {
+        System.out.println("Overridden display, Var = " + Var);
+    }
 }
