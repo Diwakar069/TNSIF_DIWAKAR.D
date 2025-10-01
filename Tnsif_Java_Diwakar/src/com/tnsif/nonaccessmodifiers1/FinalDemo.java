b4589c05ca7ee82d86e1131d89466582cb9df39d
HP <HP@LAPTOP-0OJ9S0RI.lan>
2025-10-01 17:39:00 +0530

    Day 5 nonaccess modifiers

---
+++ Tnsif_Java_Diwakar/src/com/tnsif/nonaccessmodifiers1/FinalDemo.java
@@ -0,0 +1,23 @@
+package com.tnsif.nonaccessmodifiers1;
+
+class FinalDemo {
+
+    final int Var = 100;
+
+    void display() {
+        System.out.println("show the value of Var: " + Var);
+    }
+
+    public static void main(String[] args) {
+        FinalDemo fd = new FinalDemo();
+        fd.display();
+        // fd.Var = 200; // ❌ Not allowed since Var is final
+    }
+}
+
+class Child extends FinalDemo {
+    @Override
+    void display() {
+        System.out.println("Overridden display, Var = " + Var);
+    }
+}
